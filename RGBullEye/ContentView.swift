60756dde6147be798aebcb39090437e24c83ef26
Hatice Nur OKUR <haticenurokur@gmail.com>
2020-11-26 17:41:55 +0300

    Initial Commit

---
+++ RGBullEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullEye
+//
+//  Created by Hatice Nur OKUR on 26.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
